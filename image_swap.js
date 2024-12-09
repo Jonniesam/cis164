a5beff67eb73927dc5b497f9696d31e0f10d88d3
Jonniesam <117244803+Jonniesam@users.noreply.github.com>
2024-12-08 21:55:52 -0800

    added ch 9 project, updated styles in sales ph to match other pages

---
+++ image_swap.js
@@ -0,0 +1,36 @@
+"use strict";
+
+$(document).ready(() => {
+    // Preload images
+    $("#image_list a").each((index, element) => {
+        const swappedImage = new Image();
+        swappedImage.src = $(element).attr("href");
+    });
+
+    // Set up event handlers for links
+    $("#image_list a").click(evt => {
+        // Prevent the default behavior of the link (no navigation)
+        evt.preventDefault();
+
+        const link = evt.currentTarget;
+
+        // Move focus to the clicked thumbnail immediately
+        $(link).focus();
+
+        // Fade out the current image and caption
+        $("#caption, #image").fadeOut(1000, () => {
+            // Update the image and caption after fade-out completes
+            const imageURL = $(link).attr("href");
+            $("#image").attr("src", imageURL);
+
+            const caption = $(link).attr("title");
+            $("#caption").text(caption);
+
+            // Fade in the new image and caption
+            $("#caption, #image").fadeIn(1000);
+        });
+    });
+
+    // Move focus to the first thumbnail initially
+    $("li:first-child a").focus();
+});
